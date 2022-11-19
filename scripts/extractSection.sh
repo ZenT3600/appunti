acd251b31ec3452a288b64c791d53134e197263c
ZenT3600 <matteo.leggio@tiscali.it>
2022-11-19 06:19:55 +0100

    Fixed bug on selecting last section

--- scripts/extractSection.sh
+++ scripts/extractSection.sh
@@ -8,6 +8,7 @@ SECTIONFULL=$(cat "$FILE" | grep -nF '\section{' | fzf -m)
 SECTIONLINE=$(echo "$SECTIONFULL" | cut -d':' -f 1)
 SECSECTIONFULL=$(tail -n +$(expr $SECTIONLINE + 1) "$FILE" | grep -nF '\section{')
 SECSECTIONLINE=$(echo "$SECSECTIONFULL" | cut -d':' -f 1 | head -n 1)
+test -z $SECSECTIONLINE && SECSECTIONLINE=$(expr $(cat "$FILE" | grep -nF '\end{document}' | cut -d':' -f 1) - 1)
 
 echo "Yanking from start to line $BEGINLINE"
 head -n $BEGINLINE "$FILE" > "$OUT/ext.tex"
