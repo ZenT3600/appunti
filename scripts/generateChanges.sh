527265a68069e99eb9803d23f48c9645323b32e1
ZenT3600 <matteo.leggio@tiscali.it>
2023-02-19 09:27:40 +0100

    Added script to generate changelog message

---
+++ scripts/generateChanges.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCOMM=$(git log --oneline | fzf | awk '{print $1}')
+DIFF=$(git diff $SCOMM:Notes.tex..HEAD:Notes.tex | grep '^+' | cut -c2-)
+
+#echo "Sections:"
+#echo "$DIFF" | grep -F '\section{' | cut -d "{" -f2 | cut -d "}" -f1
+#echo "SubSections:"
+#echo "$DIFF" | grep -F '\subsection{' | cut -d "{" -f2 | cut -d "}" -f1
+echo "@everyone Gli appunti sono stati aggiornati!"
+echo "Le seguenti sezioni sono state modificate/aggiunte:"
+echo ""
+echo "$DIFF" | grep -F '\subsubsection{' | cut -d "{" -f2 | cut -d "}" -f1
+echo ""
+echo "Il link al PDF e' https://raw.githubusercontent.com/ZenT3600/appunti/main/Notes.pdf"
+echo ""
+echo "||Questo messaggio e' stato generato automaticamente||"
