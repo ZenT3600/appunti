00a1bc7ddf6862ae64f1c0f4b78f943f67de35dc
ZenT3600 <matteo.leggio@tiscali.it>
2023-02-21 07:24:48 +0100

    Improved generateChanges.sh output

--- scripts/generateChanges.sh
+++ scripts/generateChanges.sh
@@ -10,7 +10,10 @@ DIFF=$(git diff $SCOMM^1:Notes.tex..HEAD:Notes.tex | grep '^+' | cut -c2-)
 echo "@everyone Gli appunti sono stati aggiornati!"
 echo "Le seguenti sezioni sono state modificate/aggiunte:"
 echo ""
-echo "$DIFF" | grep -F '\subsubsection{' | cut -d "{" -f2 | cut -d "}" -f1
+echo "$DIFF" | grep -F '\subsubsection{' | while read line; do
+	echo $line | cut -d "{" -f2 | cut -d "}" -f1 | tr -d '\n'
+	echo " ($(echo $line | awk -F' % ' '{print $2}'))"
+done
 echo ""
 echo "Il link al PDF e' https://raw.githubusercontent.com/ZenT3600/appunti/main/Notes.pdf"
 echo ""
